20d2143b4b9acba45c4fb4b251a53f8e31d6fee8
Ujjwal Talwar <mail2ujjwal98@gmail.com>
2024-02-23 23:03:26 +0530

    added starry

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
-import gon0 from "./assets/image1x1.webp";
-import gon1 from "./assets/image2x1.webp";
-import gon2 from "./assets/image3x1.webp";
-import gon3 from "./assets/image1x2.webp";
-import gon4 from "./assets/image2x2.webp";
-import gon5 from "./assets/image3x2.webp";
-import gon6 from "./assets/image1x3.webp";
-import gon7 from "./assets/image2x3.webp";
+import gon0 from "./assets/gon/image1x1.webp";
+import gon1 from "./assets/gon/image2x1.webp";
+import gon2 from "./assets/gon/image3x1.webp";
+import gon3 from "./assets/gon/image1x2.webp";
+import gon4 from "./assets/gon/image2x2.webp";
+import gon5 from "./assets/gon/image3x2.webp";
+import gon6 from "./assets/gon/image1x3.webp";
+import gon7 from "./assets/gon/image2x3.webp";
+import guts0 from "./assets/guts/image1x1.jpg";
+import guts1 from "./assets/guts/image2x1.jpg";
+import guts2 from "./assets/guts/image3x1.jpg";
+import guts3 from "./assets/guts/image1x2.jpg";
+import guts4 from "./assets/guts/image2x2.jpg";
+import guts5 from "./assets/guts/image3x2.jpg";
+import guts6 from "./assets/guts/image1x3.jpg";
+import guts7 from "./assets/guts/image2x3.jpg";
 
 const IMAGE_MAPPING = {
-  0: gon0,
-  1: gon1,
-  2: gon2,
-  3: gon3,
-  4: gon4,
-  5: gon5,
-  6: gon6,
-  7: gon7,
+  GON: {
+    0: gon0,
+    1: gon1,
+    2: gon2,
+    3: gon3,
+    4: gon4,
+    5: gon5,
+    6: gon6,
+    7: gon7,
+  },
+  GUTS: {
+    0: guts0,
+    1: guts1,
+    2: guts2,
+    3: guts3,
+    4: guts4,
+    5: guts5,
+    6: guts6,
+    7: guts7,
+  },
 };
 // questions:
 // Q1: How to know if there is free space next to piece?
@@ -26,8 +46,9 @@ const IMAGE_MAPPING = {
 // A2: Maybe by looking at the state, need to have some kind of structure
 function App() {
   const [clicks, setClicks] = useState(0); // amount of clicks taken by user to reach the end of the game
-  const [puzzleState, setPuzzleState] = useState([0, 1, 2, 3, 8, 4, 6, 7, 5]); // state of the puzzle it start with pieces in place 1-8 with 9 being 0
+  const [puzzleState, setPuzzleState] = useState(createRandomArray()); // state of the puzzle it start with pieces in place 1-8 with 9 being 0
   const [isPuzzleSolved, setIsPuzzleSolved] = useState(false);
+  const [currentImage, setCurrentImage] = useState("GON");
   function checkForFreeSpace(selectedPiece) {
     if (
       (selectedPiece == 2 && puzzleState.indexOf(8) == 3) ||
@@ -84,7 +105,11 @@ function App() {
   function reset() {
     setClicks(0);
     setIsPuzzleSolved(false);
-    setPuzzleState([0, 2, 5, 6, 7, 3, 4, 8, 1]);
+    setPuzzleState(createRandomArray());
+  }
+  function changeImage() {
+    setCurrentImage("GUTS");
+    reset();
   }
   // checkForFinish();
   useEffect(() => {
@@ -94,11 +119,13 @@ function App() {
   // console.log(puzzleState, `freespacestate ${freeSpaceState}`, isPuzzleSolved);
   return (
     <>
+      <button onClick={() => changeImage()}>CHANGE IMAGE</button>
       <PuzzleBoard
         clicks={clicks}
         setClicks={setClicks}
         swap={swap}
         puzzleState={puzzleState}
+        currentImage={currentImage}
       ></PuzzleBoard>
       {isPuzzleSolved ? (
         <h1 className="winning-banner">
@@ -114,7 +141,7 @@ function App() {
   );
 }
 
-function PuzzleBoard({ clicks, setClicks, swap, puzzleState }) {
+function PuzzleBoard({ clicks, setClicks, swap, puzzleState, currentImage }) {
   return (
     <>
       <div className="container">
@@ -129,7 +156,7 @@ function PuzzleBoard({ clicks, setClicks, swap, puzzleState }) {
                   onClick={() => swap(idx)}
                 >
                   <img
-                    src={IMAGE_MAPPING[val]}
+                    src={IMAGE_MAPPING[currentImage][val]}
                     alt="gonnnnn"
                     className="puzzle-picture"
                   ></img>
@@ -155,7 +182,7 @@ function PuzzleBoard({ clicks, setClicks, swap, puzzleState }) {
                   onClick={() => swap(idx + 3)}
                 >
                   <img
-                    src={IMAGE_MAPPING[val]}
+                    src={IMAGE_MAPPING[currentImage][val]}
                     alt="gonnnnn"
                     className="puzzle-picture"
                   ></img>
@@ -182,7 +209,7 @@ function PuzzleBoard({ clicks, setClicks, swap, puzzleState }) {
                   onClick={() => swap(idx + 6)}
                 >
                   <img
-                    src={IMAGE_MAPPING[val]}
+                    src={IMAGE_MAPPING[currentImage][val]}
                     alt="gonnnnn"
                     className="puzzle-picture"
                   ></img>
@@ -202,4 +229,17 @@ function PuzzleBoard({ clicks, setClicks, swap, puzzleState }) {
   );
 }
 
+function createRandomArray() {
+  const arrayLength = 9;
+  const uniqueNumbers = Array.from({ length: arrayLength }, (_, i) => i);
+  const randomArray = [];
+
+  while (uniqueNumbers.length > 0) {
+    const randomIndex = Math.floor(Math.random() * uniqueNumbers.length);
+    const randomValue = uniqueNumbers.splice(randomIndex, 1)[0];
+    randomArray.push(randomValue);
+  }
+
+  return randomArray;
+}
 export default App;
